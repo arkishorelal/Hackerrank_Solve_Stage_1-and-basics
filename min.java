e5ef171152e47dd90b2cde4006c719af429f4e63
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2022-09-10 13:30:48 +0530

    Update min.java

--- min.java
+++ min.java
@@ -1,3 +1,32 @@
+/*Find the minimum value in an array.
+
+Input Format
+
+Input consists one integer and one array
+
+Constraints
+
+1 <= N <= 100 -1000 <= arr[i] <= 1000
+
+Output Format
+
+Output should be the minimum element in an array
+
+Sample Input 0
+
+5
+1 3 2 7 6
+Sample Output 0
+
+1
+Sample Input 1
+
+7
+7 3 0 2 5 6 2
+Sample Output 1
+
+0  */
+
 package hackerrank;
 
 import java.util.Scanner;
