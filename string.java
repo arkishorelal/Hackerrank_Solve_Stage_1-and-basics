0be633946ce10d4316bdcbd3d54b7afa3726d93d
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2022-09-10 13:32:09 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,3 +1,31 @@
+/*Print the sum by adding all the characters by using their ASCII value.
+
+Input Format
+
+Input should be String
+
+Constraints
+
+No constraints
+
+Output Format
+
+Output should be an Integer
+
+Sample Input 0
+
+goal
+Sample Output 0
+
+419
+Sample Input 1
+
+act
+Sample Output 1
+
+312
+*/ 
+
 package hackerrank;
 
 import java.util.Scanner;
