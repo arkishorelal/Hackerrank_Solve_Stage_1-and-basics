0d5dbd0e62fbbbd2338284398ddf0586041e7c7c
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2022-09-10 13:26:11 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,3 +1,41 @@
+/*Print the pattern as given below
+
+Input Format
+
+It should be an Integer
+
+Constraints
+
+1<= N <= 100
+
+Output Format
+
+Print the pattern
+
+Sample Input 0
+
+3
+Sample Output 0
+
+abc
+ab
+a
+Sample Input 1
+
+5
+Sample Output 1
+
+abcde
+abcd
+abc
+ab
+a*/
+
+
+
+
+
+
 package hackerrank;
 
 import java.util.Scanner;
