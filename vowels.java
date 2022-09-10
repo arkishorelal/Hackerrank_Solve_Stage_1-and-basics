fe4b8b0cc4e8e33703ef0c7952e3a7d91be8846b
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2022-09-10 13:33:28 +0530

    Update vowels.java

--- vowels.java
+++ vowels.java
@@ -1,3 +1,41 @@
+/*The five letters A, E, I, O and U are called vowels. All other alphabets except these 5 vowels are called consonants.
+
+Tony is studying fifth standard. In school he learns about vowels and consonants. At first he didn't catch up. Tony is a curious boy. After school, he comes to you and asks you to teach him. You want to teach Tony using your programming skills. Let's write a program.
+
+Assume that the user will always enter an alphabet character.
+
+Input Format
+
+a
+E
+
+Constraints
+
+'a' <= input <= 'z'
+'A' <= input <= 'Z'
+
+Note : If vovel - print 'vowel' in lowercase exactly
+If consonant - print 'consonant' in lowercase exactly.
+
+Output Format
+
+consonant
+vowel
+
+Sample Input 0
+
+a
+Sample Output 0
+
+vowel
+Sample Input 1
+
+K
+Sample Output 1
+
+consonant
+*/
+
 package hackerrank;
 
 import java.util.Scanner;
