0f70e43fb94c4559e4bcc3df96b85bea4b7d43bc
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2022-09-10 13:29:21 +0530

    Update even.java

--- even.java
+++ even.java
@@ -1,3 +1,34 @@
+/*Reverse the digit. Take only even digits while reversing.
+
+Input Format
+
+Input should be an Integer
+
+Constraints
+
+1 <= N <= 1000000
+
+Output Format
+
+Output should be an Integer
+
+Sample Input 0
+
+124
+Sample Output 0
+
+42
+Sample Input 1
+
+214896
+Sample Output 1
+
+6842*/
+
+
+
+
+
 package hackerrank;
 
 import java.util.Scanner;
